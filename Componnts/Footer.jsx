f777bdbff3faef5aec2287a2835cd5d8d8f2745f
shiva moeini <87569940+shivamoeini@users.noreply.github.com>
2023-05-02 18:49:47 +0430

    full project

---
+++ Componnts/Footer.jsx
@@ -0,0 +1,14 @@
+
+import styles from '../styles/Footer.module.css';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+Copyright <span className={styles.brand}>GamesKart</span>{' '}
+      {new Date().getFullYear()}
+
+    </footer>
+  )
+}
+
+export default Footer
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
-
+import { Provider } from 'react-redux';  
+import Navbar from '@/Componnts/Navbar'
+import Footer  from "@/Componnts/Footer"
+import store from '../redux/store'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <Provider store={store}>
+    <div className="wrapper">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  </Provider>
+  )
 }
